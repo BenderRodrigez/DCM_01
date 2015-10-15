c9aba8f45283c265d611dc5447ad544bf4f29493
Равиль <mr.makoto.kun@gmail.com>
2015-10-15 23:11:13 +0400

    Some usefull version

--- DCM_1/DCM_1/MainWindow.xaml.cs
+++ DCM_1/DCM_1/MainWindow.xaml.cs
@@ -1,26 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using DCM_1.Annotations;
 using Microsoft.Win32;
-using NeuronDotNet.Core;
-using NeuronDotNet.Core.Initializers;
-using NeuronDotNet.Core.SOM;
-using NeuronDotNet.Core.SOM.NeighborhoodFunctions;
 using Color = System.Drawing.Color;
 
 namespace DCM_1
@@ -28,12 +20,18 @@ namespace DCM_1
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : INotifyPropertyChanged
     {
         public ImageSource SourceImage { get; set; }
-        public ImageSource ResultImage { get; set; }
+        public BitmapImage ResultImage { get; set; }
+        public string PerformanceComparison { get; set; }
         private string _fileName;
-        private KohonenNetwork _network;
+        private VectorQuantization _vq;
+        private Stopwatch _sompressStopWath;
+        private Stopwatch _decompressStopWath;
+        private readonly OpenFileDialog _openDialog = new OpenFileDialog();
+        private CompressedImage _compressed;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -41,110 +39,101 @@ namespace DCM_1
 
         private void OpenButton_OnClick(object sender, RoutedEventArgs e)
         {
-            var openDialog = new OpenFileDialog();
-            if (openDialog.ShowDialog(this).Value)
+            if (_openDialog.ShowDialog(this).Value)
             {
-                _fileName = openDialog.FileName;
-                SourceImage = new BitmapImage(new Uri(openDialog.FileName));
-                OnPropertyChanged("SourceImage");
-                var bitmap = new Bitmap(_fileName);
-                var image = new double[bitmap.Width*bitmap.Height][];
-                var k = 0;
-                for (int i = 0; i < bitmap.Width; i++)
-                {
-                    for (int j = 0; j < bitmap.Height; j++)
-                    {
-                        var color = bitmap.GetPixel(i, j);
-                        image[k] = new[] {(double) color.R, (double) color.B, (double) color.G};
-                        k++;
-                    }
-                }
-
-                ProvideKohonenCom(image);
-
-                var compressed = new CompressedImage
-                {
-                    Height = bitmap.Height,
-                    Width = bitmap.Width,
-                    CodeBook = new byte[64][]
-                };
-                var cb = 0;
-                foreach (var neuron in _network.OutputLayer.Neurons)
-                {
-                    compressed.CodeBook[cb] = new[]
-                    {
-                        (byte) Math.Round(neuron.SourceSynapses[0].Weight), (byte) Math.Round(neuron.SourceSynapses[1].Weight),
-                        (byte) Math.Round(neuron.SourceSynapses[2].Weight)
-                    };
-                    cb++;
-                }
-
-                compressed.Image = new byte[bitmap.Width*bitmap.Height];
-                k = 0;
-                for (int i = 0; i < bitmap.Width; i++)
-                {
-                    for (int j = 0; j < bitmap.Height; j++)
-                    {
-                        var color = bitmap.GetPixel(i, j);
-                        _network.Run(new[] {(double) color.R, (double) color.B, (double) color.G});
-                        
-                        compressed.Image[k] = (byte)(_network.Winner.Coordinate.Y*bitmap.Width+_network.Winner.Coordinate.X);
-                        k++;
-                    }
-                }
-
-                var comprBitmap = new Bitmap(bitmap.Width, bitmap.Height);
-                k = 0;
-                for (int i = 0; i < comprBitmap.Width; i++)
-                {
-                    for (int j = 0; j < comprBitmap.Height; j++)
-                    {
-                        comprBitmap.SetPixel(i, j, Color.FromArgb(0, compressed.CodeBook[k][0], compressed.CodeBook[k][1], compressed.CodeBook[k][2]));
-                    }
-                }
-                //ResultImage = new DrawingImage();
+                SourceImage = new BitmapImage(new Uri(_openDialog.FileName));
+                ResultImage = new BitmapImage();
+                var task = new Task(Compress);
+                task.Start();
             }
         }
 
-        private bool[,] ProvideKohonenCom(double[][] trainingSet)
+        private void Decompress()
         {
-            var outputLayerSize = 8;
-            var isWinner = new bool[outputLayerSize, outputLayerSize];
-            var learningRadius = outputLayerSize / 2;
-            var neigborhoodFunction = new GaussianFunction(learningRadius);
-            const LatticeTopology topology = LatticeTopology.Hexagonal;
-            var max = trainingSet.Max(x => x.Max());
-            var min = trainingSet.Min(x => x.Min());
-            var inputLayer = new KohonenLayer(trainingSet[0].Length);
-            var outputLayer = new KohonenLayer(new System.Drawing.Size(outputLayerSize, outputLayerSize), neigborhoodFunction, topology);
-            new KohonenConnector(inputLayer, outputLayer) { Initializer = new RandomFunction(min, max) };
-            outputLayer.SetLearningRate(0.2, 0.05d);
-            outputLayer.IsRowCircular = false;
-            outputLayer.IsColumnCircular = false;
-            _network = new KohonenNetwork(inputLayer, outputLayer);
-
-            var progress = 1;
-            _network.BeginEpochEvent += (senderNetwork, args) => Array.Clear(isWinner, 0, isWinner.Length);
-
-            _network.EndSampleEvent += delegate
+            _decompressStopWath = new Stopwatch();
+            _decompressStopWath.Start();
+            var comprBitmap = new Bitmap(_compressed.Width, _compressed.Height);
+            var k = 0;
+            for (int i = 0; i < comprBitmap.Width; i++)
             {
-                isWinner[_network.Winner.Coordinate.X, _network.Winner.Coordinate.Y] =
-                    true;
-            };
-
-            _network.EndEpochEvent += delegate
-            {
-                /*progressBar1.Value = ((progress++) * 100) / 500;
-                PlotWinnersNeurons(isWinner);
-                Application.DoEvents();*/
-            };
-            var trSet = new TrainingSet(trainingSet[0].Length);
-            foreach (var x in trainingSet)
-            {
-                trSet.Add(new TrainingSample(x));
+                for (int j = 0; j < comprBitmap.Height; j++)
+                {
+                    var pos = _compressed.Image[k];
+                    comprBitmap.SetPixel(i, j,
+                        Color.FromArgb(_compressed.CodeBook[pos][3], _compressed.CodeBook[pos][0], _compressed.CodeBook[pos][2],
+                            _compressed.CodeBook[pos][1]));
+                    k++;
+                }
             }
-            _network.Learn(trSet, 500);
-            return isWinner;
+            _decompressStopWath.Stop();
+            var stream = new MemoryStream();
+            comprBitmap.Save(stream, ImageFormat.Png);
+            ResultImage.BeginInit();
+            ResultImage.StreamSource = stream;
+            ResultImage.EndInit();
+            OnPropertyChanged("ResultImage");
+        }
+
+        private void Compress()
+        {
+            _sompressStopWath = new Stopwatch();
+            _fileName = _openDialog.FileName;
+            OnPropertyChanged("SourceImage");
+            var bitmap = new Bitmap(_fileName);
+            _sompressStopWath.Start();
+            var k = 0;
+            var colors = new List<Color>();
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    var color = bitmap.GetPixel(i, j);
+                    colors.Add(color);
+                    k++;
+                }
+            }
+
+            var image = colors.GroupBy(x => x)
+                .Select(x => new[] {(double) x.Key.R, (double) x.Key.B, (double) x.Key.G, (double) x.Key.A})
+                .AsParallel()
+                .ToArray();
+
+            _vq = new VectorQuantization(image, 4, 64);
+
+            _compressed = new CompressedImage
+            {
+                Height = bitmap.Height,
+                Width = bitmap.Width,
+                CodeBook =
+                    _vq.CodeBook.Select(
+                        x =>
+                            new[]
+                            {
+                                (byte) Math.Round(x[0]), (byte) Math.Round(x[1]), (byte) Math.Round(x[2]),
+                                (byte) Math.Round(x[3])
+                            }).ToArray(),
+                Image = new byte[bitmap.Width*bitmap.Height]
+            };
+            k = 0;
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    var color = bitmap.GetPixel(i, j);
+                    var pos = _vq.QuantazationIndex(new[] {(double) color.R, color.B, color.G, color.A});
+                    _compressed.Image[k] = (byte) pos;
+                    k++;
+                }
+            }
+            _sompressStopWath.Stop();
+            Wind.Dispatcher.Invoke(Decompress);
+            Wind.Dispatcher.Invoke(SetResults);
+        }
+
+        private void SetResults()
+        {
+            var val = _sompressStopWath.ElapsedMilliseconds/(double)_decompressStopWath.ElapsedMilliseconds;
+            PerformanceComparison = string.Format("1/{0:F3}", val);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
---
+++ DCM_1/DCM_1/VectorQuantization.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DCM_1
+{
+    /// <summary>
+    /// Производит векторное квантование вектора признаков для диктора. Использует алгоритм ЛБГ
+    /// </summary>
+    public class VectorQuantization
+    {
+        /// <summary>
+        /// Кодовая книга, на основе которой будет происходить квантование
+        /// </summary>
+        public double[][] CodeBook;
+
+        /// <summary>
+        /// Выборка векторов признаков, на основе которых будет производиться обучение кодовой книги
+        /// </summary>
+        public readonly double[][] TrainingSet;
+
+        /// <summary>
+        /// Размер кодовой книги, по которой будем выявлять диктора
+        /// </summary>
+        private readonly int _codeBookSize;
+
+        /// <summary>
+        /// Погрешность квантования
+        /// </summary>
+        public double DistortionDelta { get; set; }
+
+        public double AverageDistortionMeasure { get; set; }
+
+        public double DistortionDispertion { get; set; }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="VectorQuantization"/>.
+        /// </summary>
+        /// <param name="traningSet">Обущающая выборка</param>
+        /// <param name="vectorLength"></param>
+        /// <param name="codeBookSize"></param>
+        public VectorQuantization(double[][] traningSet, int vectorLength, int codeBookSize)
+        {
+            _codeBookSize = codeBookSize;
+			TrainingSet = traningSet;
+            DistortionDelta = 0.05;
+            CodeBookInit(vectorLength);
+            DistortionDispertion = DistortionMeasureDispersion();
+		}
+
+        private double DistortionMeasureDispersion()
+        {
+            double msquare = 0.0;
+            for(int i = 0; i < TrainingSet.Length; i++)
+            {
+                msquare += Math.Pow(QuantizationError(TrainingSet[i], Quantazation(TrainingSet[i])) - AverageDistortionMeasure, 2);
+            }
+            msquare /= (TrainingSet.Length);
+
+            return msquare;
+        }
+
+		/// <summary>
+		/// Инициализирует кодовую книгу для заданного набора обучающих значений
+		/// </summary>
+        private void CodeBookInit(int vectorLength)
+        {
+            int iteration = 1;//текущая итерация обучения
+            //Что-то вроде инициализации начальных условий
+            CodeBook = new double[iteration][];
+            CodeBook[0] = new double[vectorLength];
+            for (int j = 0; j < vectorLength; j++)
+            {//идём по каждому элементу вектора
+                var j1 = j;
+                var res = Parallel.For(0, TrainingSet.Length, i =>
+                    CodeBook[0][j1] += TrainingSet[i][j1]);//добавляем значения из обучающей выборки
+                while (!res.IsCompleted)
+                {
+                    
+                }
+                CodeBook[0][j] /= TrainingSet.Length;
+            }
+            var averageQuantError = AverageQuantizationError();
+
+		    while (iteration < _codeBookSize)
+            {
+                var newCodeBook = new double[iteration*2][];
+                Parallel.For(0, CodeBook.Length, cb =>
+                {
+                    var maxDistance = double.NegativeInfinity;
+                    var centrOne = new double[vectorLength];
+                    var centrTwo = new double[vectorLength];
+                    for (int i = 0; i < TrainingSet.Length-1; i++)
+                    {
+                        if (QuantazationIndex(TrainingSet[i]) != cb) continue;
+
+                        for (int j = i+1; j < TrainingSet.Length; j++)
+                        {
+                            if (QuantazationIndex(TrainingSet[j]) != cb) continue;
+
+                            var distance = QuantizationError(TrainingSet[i], TrainingSet[j]);
+                            if (distance > maxDistance)
+                            {
+                                centrOne = TrainingSet[i];
+                                centrTwo = TrainingSet[j];
+                                maxDistance = distance;
+                            }
+                        }
+                    }
+
+                    newCodeBook[(cb + 1)*2 - 1] = new double[vectorLength];
+                    newCodeBook[(cb + 1) * 2 - 2] = new double[vectorLength];
+
+                    if (centrOne.Sum(x => (int)x) == 0)
+                    {
+                        var rand = new Random();
+                        for (int i = 0; i < vectorLength; i++)
+                        {
+                            newCodeBook[(cb + 1)*2 - 1][i] = CodeBook[cb][i] - CodeBook[cb][i]*rand.NextDouble()*0.1;
+                        }
+                    }
+                    else
+                    {
+                        Array.Copy(centrOne, newCodeBook[(cb + 1)*2 - 1], vectorLength);
+                    }
+                    if (centrTwo.Sum(x => (int)x) == 0)
+                    {
+                        var rand = new Random();
+                        for (int i = 0; i < vectorLength; i++)
+                        {
+                            newCodeBook[(cb + 1)*2 - 2][i] = CodeBook[cb][i] + CodeBook[cb][i]*rand.NextDouble()*0.1;
+                        }
+                    }
+                    else
+                    {
+                        Array.Copy(centrTwo, newCodeBook[(cb + 1)*2 - 2], vectorLength);
+                    }
+                });
+
+                iteration *= 2;
+                CodeBook = newCodeBook;
+
+                //D(m-1) - Dm > DistortionDelta?
+                var averageQuantErrorOld = averageQuantError;
+                averageQuantError = AverageQuantizationError();
+                var kMeansIntertionsCount = 0;
+                while(Math.Abs(averageQuantErrorOld - averageQuantError) > DistortionDelta && kMeansIntertionsCount < 50)//abs here
+                {
+                    //yi = total_sum(xi)/N
+					var tmpCodeBook = new double[CodeBook.Length][];
+                    var vectorsCount = new int[CodeBook.Length];
+                    var res = Parallel.For(0, TrainingSet.Length, i =>
+                    {
+                        int codeBookIndex = QuantazationIndex(TrainingSet[i]);
+                        if (tmpCodeBook[codeBookIndex] == null)
+                            tmpCodeBook[codeBookIndex] = new double[vectorLength];
+                        for (int j = 0; j < vectorLength; j++)
+                        {
+                            tmpCodeBook[codeBookIndex][j] += TrainingSet[i][j];
+                        }
+                        vectorsCount[codeBookIndex]++;
+                    });
+
+                    while (!res.IsCompleted)
+                    {
+                        
+                    }
+
+                    var result = Parallel.For(0, tmpCodeBook.Length, i =>
+                    {
+                        if (tmpCodeBook[i] == null)
+                        {
+                            tmpCodeBook[i] = new double[vectorLength];
+                            Array.Copy(CodeBook[i], tmpCodeBook[i], vectorLength);
+                        }
+                        else if (vectorsCount[i] > 0)
+                        {
+                            for (int j = 0; j < tmpCodeBook[i].Length; j++)
+                                tmpCodeBook[i][j] /= vectorsCount[i];
+                        }
+                    });
+                    while (!result.IsCompleted)
+                    {
+                        
+                    }
+                    CodeBook = tmpCodeBook;
+                    averageQuantErrorOld = averageQuantError;
+                    averageQuantError = AverageQuantizationError();
+                    kMeansIntertionsCount++;
+                }
+            }
+            AverageDistortionMeasure = AverageQuantizationError();
+        }
+
+		/// <summary>
+		/// Вычисляет среднюю ошибку квантования для кодовой книги
+		/// </summary>
+		/// <returns>Средняя ошибка квантования</returns>
+        private double AverageQuantizationError()
+        {//D=(total_sum(d(x, Q(x))))/N
+            double errorRate = 0;
+            for (int i = 0; i < TrainingSet.Length; i++)
+                errorRate += QuantizationError(TrainingSet[i], Quantazation(TrainingSet[i]));
+            errorRate /= TrainingSet.Length;
+            return errorRate;
+        }
+
+		/// <summary>
+		/// Оператор векторного квантования
+		/// </summary>
+		/// <param name="x">Вектор входных значений</param>
+        public double[] Quantazation(double[] x)
+        {//Оператор квантования
+		    var minError = double.PositiveInfinity;
+            int min = 0;
+            for (int i = 0; i < CodeBook.Length; i++)
+            {
+                var error = QuantizationError(x, CodeBook[i]);
+                if (error < minError)
+                {
+                    min = i;
+                    minError = error;
+                }
+            }
+		    return CodeBook[min];
+        }
+
+		/// <summary>
+		/// Возвращает индекс центроида для данного вектора
+		/// </summary>
+		/// <returns>Индекс в кодовой книге</returns>
+		/// <param name="x">Вектор сходных значений</param>
+		public int QuantazationIndex(double[] x)
+		{//Тоже, что и оператор квантования, но возвращает индекс в книге
+		    var minError = double.PositiveInfinity;
+			int min = 0;
+			for (int i = 0; i < CodeBook.Length; i++)
+			{
+			    var error = QuantizationError(x, CodeBook[i]);
+
+			    if (!(error < minError)) continue;
+
+			    min = i;
+			    minError = error;
+			}
+		    return min;
+		}
+
+		/// <summary>
+		/// Расчитывает ошибку между двумя векторами
+		/// </summary>
+		/// <returns>The error.</returns>
+		/// <param name="a">The alpha component.</param>
+		/// <param name="b">The blue component.</param>
+        public static double QuantizationError(double[] a, double[] b)
+        {//d=total_sum(a^2-b^2)
+		    if (a.Length == b.Length)
+            {
+                var error = a.Select((t, i) => Math.Pow(t - b[i], 2)).Sum();
+                return error;
+            }
+		    throw new Exception("Вектора разной длины!");
+        }
+    }
+}
